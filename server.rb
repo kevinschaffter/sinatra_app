abf50881ed2156a1f52435c7e59cee47ed9940b7
Kevin Schaffter <kevinschaffter@gmail.com>
2017-11-08 20:19:21 -0500

    added thing

--- server.rb
+++ server.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
+require 'HTTParty'
+require 'Nokogiri'
 
 get '/' do
 
-  redirect '/hello.txt'
-  
+  if params[:job]
+    @job = params[:job]
+  else 
+    @job = "sof"
+  end
+
+  url = "https://miami.craigslist.org/search/#{@job}"
+  response = HTTParty.get(url)
+  html = response.body
+  dom = Nokogiri::HTML(html)
+  @links = dom.css("a.hdrlnk")
+
+  @i = 1
+
+  erb :thing
+
 end
+
+
